1fa2c5c9efbdc9b8654050872cf4a82a9a1f36df
sandeshugale <sugale32@gmail.com>
2018-08-13 12:10:26 +0530

    change by sandesh

--- DiagonalLayout-master/app/src/main/java/com/github/florent37/sample/diagonallayout/DiagonalLayoutMainActivity.java
+++ DiagonalLayout-master/app/src/main/java/com/github/florent37/sample/diagonallayout/DiagonalLayoutMainActivity.java
@@ -1,5 +1,6 @@
 package com.github.florent37.sample.diagonallayout;
 
+import android.media.session.PlaybackState;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -7,7 +8,7 @@ import android.support.v7.widget.Toolbar;
 public class DiagonalLayoutMainActivity extends AppCompatActivity {
 
     Toolbar toolbar;
-
+//    PlaybackState.CustomAction
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
